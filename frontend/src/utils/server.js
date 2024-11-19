fce19a858c1d83523d7707ee7571b14bb6dc5fb9
Yuvraj <yuvrajs22it@student.mes.ac.in>
2024-11-19 20:28:33 +0530

    in frontend server updated to deployment.

--- frontend/src/utils/server.js
+++ frontend/src/utils/server.js
@@ -1,1 +1,2 @@
-export const SERVER = "http://localhost:3000/api/v1";
+// export const SERVER = "http://localhost:3000/api/v1";
+export const SERVER = "https://food-unity-backend.vercel.app/api/v1"; // for deployment
